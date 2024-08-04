fff715787939923f09fc71583039884a7631f670
zoooi04 <ooicc-pp22@student.tarc.edu.my>
2024-08-04 16:14:55 +0800

    Update Charity.java

--- Charity/src/charity/Charity.java
+++ Charity/src/charity/Charity.java
@@ -15,10 +15,6 @@ public class Charity {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        // testing mearge
-        System.out.println("new line added!!!");
-
     }
     
 }
