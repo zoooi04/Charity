0d8de7d93a5f84a608db8eed2dde787ac2246875
HP <HP@DESKTOP-M3U7TNL>
2024-08-04 21:39:55 +0800

    updated

--- Charity/src/charity/Charity.java
+++ Charity/src/charity/Charity.java
@@ -16,7 +16,7 @@ public class Charity {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("test");
+        System.out.println("test1");
         System.out.println("testing");
     }
     
