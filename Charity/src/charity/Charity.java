412207ccfdd905cbb4014ada01d5cab2844b325b
Zoooi9918 <87220843+Zoooi9918@users.noreply.github.com>
2024-08-04 16:24:33 +0800

    update Charity.java

--- Charity/src/charity/Charity.java
+++ Charity/src/charity/Charity.java
@@ -15,7 +15,6 @@ public class Charity {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
     }
     
 }
